1cd970b471a99161a2ab562d288c3840385bbd5a
AlbertoContaldi <dark.ac41@gmail.com>
2025-06-13 16:58:08 +0200

    prova

--- Main.py
+++ Main.py
@@ -2,19 +2,35 @@ import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from Classes.PieChart import PieChartFrame
-#importazione per il grafico cartesiano:
+from Classes.graficoCartesiano import CartesianPlotFrame  # <-- grafico XY
+
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
+        super().__init__(parent, *args, **kwargs)
         self.parent = parent
-        self.pieChart1 = PieChartFrame(parent,'Test1')
-        self.pieChart2 = PieChartFrame(parent,'Test2')
+
+        # 1. --- Grafico cartesiano in alto ---
+        self.cartesianChart = CartesianPlotFrame(self, title="Grafico XY",
+                                                 x_data=[0, 1, 2, 3, 4],
+                                                 y_data=[0, 1, 4, 9, 16])
+        self.cartesianChart.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        # 2. --- Frame contenitore per le due torte ---
+        bottom_frame = tk.Frame(self)
+        bottom_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        # 3. --- Due grafici a torta affiancati ---
+        self.pieChart1 = PieChartFrame(bottom_frame, 'Test1')
+        self.pieChart2 = PieChartFrame(bottom_frame, 'Test2')
 
         self.pieChart1.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
         self.pieChart2.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
 
 
-root = tk.Tk()
-MainApplication(root).pack(side="top", fill="both", expand=True)
-root.mainloop()
+# Avvio applicazione
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Grafico XY sopra le Torte")
+    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
